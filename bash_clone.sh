8acf2bc1b597c41c72a5b6c0de213543ee2f47ea
Nikola <nmarinosraitsevits@ucsd.edu>
2024-01-13 12:31:01 -0800

    bash file

---
+++ bash_clone.sh
@@ -0,0 +1,12 @@
+cd src/
+
+# Directories to be cloned
+git clone https://github.com/ros-planning/moveit2.git
+git clone https://github.com/PickNikRobotics/launch_param_builder.git
+git clone https://github.com/ros-planning/moveit2_tutorials.git
+git clone https://github.com/ros-planning/moveit_resources.git
+git clone https://github.com/ros-planning/moveit_task_constructor.git
+git clone https://github.com/ros-planning/moveit_visual_tools.git
+git clone https://github.com/PickNikRobotics/rosparam_shortcuts.git
+git clone https://github.com/ros-planning/srdfdom.git
+git clone https://github.com/xArm-Developer/xarm_ros2.git
