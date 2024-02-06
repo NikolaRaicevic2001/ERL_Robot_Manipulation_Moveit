58bf8ead81a6a06a51fd9241e38106150ab9048c
Nikola <nmarinosraitsevits@ucsd.edu>
2024-02-06 13:57:48 -0800

    Adding obstacles

--- src/my_moveit/src/xarm6_moveit_target_pose_colobj.cpp
+++ src/my_moveit/src/xarm6_moveit_target_pose_colobj.cpp
@@ -1,28 +1,32 @@
-#include <signal.h>
-#include <thread>
+#include <memory>
 #include <rclcpp/rclcpp.hpp>
+#include <thread>  
 
-#include <std_msgs/msg/bool.hpp>
-#include <xarm_msgs/srv/plan_pose.hpp>
-#include <xarm_msgs/srv/plan_joint.hpp>
-#include <xarm_msgs/srv/plan_exec.hpp>
-#include <xarm_msgs/srv/plan_single_straight.hpp>
-
+#include <moveit/move_group_interface/move_group_interface.h>
 #include <moveit/planning_scene_interface/planning_scene_interface.h>
+#include <moveit_visual_tools/moveit_visual_tools.h>
 
-#define SERVICE_CALL_FAILED 999
-
-std::shared_ptr<rclcpp::Node> node;
-
-void exit_sig_handler(int signum)
+int main(int argc, char * argv[])
 {
-    fprintf(stderr, "[test_xarm_planner_node_pose] Ctrl-C caught, exit process...\n");
-    exit(-1);
-}
+  // Initialize ROS and create the Node
+  rclcpp::init(argc, argv);
+  auto const node = std::make_shared<rclcpp::Node>("my_moveit", rclcpp::NodeOptions().automatically_declare_parameters_from_overrides(true));
 
-// Create collision object for the robot to avoid
-auto const collision_object = [frame_id =
-                                 move_group_interface.getPlanningFrame()] {
+  // Create a ROS logger
+  auto const logger = rclcpp::get_logger("my_moveit");
+  
+  // Spin up a SingleThreadedExecutor for MoveItVisualTools to interact with ROS
+  rclcpp::executors::SingleThreadedExecutor executor;
+  executor.add_node(node);
+  auto spinner = std::thread([&executor]() { executor.spin(); });
+
+  // Create the MoveIt MoveGroup Interface
+  using moveit::planning_interface::MoveGroupInterface;
+  auto move_group_interface = MoveGroupInterface(node, "xarm6");
+  
+  
+  // Create collision object for the robot to avoid
+auto const collision_object = [frame_id = move_group_interface.getPlanningFrame()] {
   moveit_msgs::msg::CollisionObject collision_object;
   collision_object.header.frame_id = frame_id;
   collision_object.id = "box1";
@@ -32,15 +36,15 @@ auto const collision_object = [frame_id =
   primitive.type = primitive.BOX;
   primitive.dimensions.resize(3);
   primitive.dimensions[primitive.BOX_X] = 0.5;
-  primitive.dimensions[primitive.BOX_Y] = 0.1;
+  primitive.dimensions[primitive.BOX_Y] = 0.5;
   primitive.dimensions[primitive.BOX_Z] = 0.5;
 
   // Define the pose of the box (relative to the frame_id)
   geometry_msgs::msg::Pose box_pose;
   box_pose.orientation.w = 1.0;  // We can leave out the x, y, and z components of the quaternion since they are initialized to 0
-  box_pose.position.x = 0.2;
-  box_pose.position.y = 0.2;
-  box_pose.position.z = 0.25;
+  box_pose.position.x = 0.25;
+  box_pose.position.y = 0.0;
+  box_pose.position.z = -0.42;
 
   collision_object.primitives.push_back(primitive);
   collision_object.primitive_poses.push_back(box_pose);
@@ -49,111 +53,41 @@ auto const collision_object = [frame_id =
   return collision_object;
 }();
 
-template<typename ServiceT, typename SharedRequest = typename ServiceT::Request::SharedPtr>
-int call_request(std::shared_ptr<ServiceT> client, SharedRequest req)
-{
-    bool is_try_again = false;
-    while (!client->wait_for_service(std::chrono::seconds(1))) {
-        if (!rclcpp::ok()) {
-            RCLCPP_ERROR(node->get_logger(), "Interrupted while waiting for the service. Exiting.");
-            exit(1);
-        }
-        if (!is_try_again) {
-            is_try_again = true;
-            RCLCPP_WARN(node->get_logger(), "service %s not available, waiting ...", client->get_service_name());
-        }
-    }
-    auto result_future = client->async_send_request(req);
-    if (rclcpp::spin_until_future_complete(node, result_future) != rclcpp::FutureReturnCode::SUCCESS)
-    {
-        RCLCPP_ERROR(node->get_logger(), "Failed to call service %s", client->get_service_name());
-        return SERVICE_CALL_FAILED;
-    }
-    auto res = result_future.get();
-    RCLCPP_INFO(node->get_logger(), "call service %s, success=%d", client->get_service_name(), res->success);
-    return res->success;
-}
+auto const collision_object_2 = [frame_id = move_group_interface.getPlanningFrame()] {
+  moveit_msgs::msg::CollisionObject collision_object;
+  collision_object.header.frame_id = frame_id;
+  collision_object.id = "box2";
+  shape_msgs::msg::SolidPrimitive primitive;
 
-int main(int argc, char** argv)
-{   
-    rclcpp::init(argc, argv);
-    rclcpp::NodeOptions node_options;
-    node_options.automatically_declare_parameters_from_overrides(true);    
-    node = rclcpp::Node::make_shared("test_xarm_planner_node_pose", node_options);
-    RCLCPP_INFO(node->get_logger(), "test_xarm_planner_node_pose start");
-    signal(SIGINT, exit_sig_handler);
+  // Define the size of the box in meters
+  primitive.type = primitive.BOX;
+  primitive.dimensions.resize(3);
+  primitive.dimensions[primitive.BOX_X] = 0.15;
+  primitive.dimensions[primitive.BOX_Y] = 0.05;
+  primitive.dimensions[primitive.BOX_Z] = 0.45;
 
-    int dof;
-    node->get_parameter_or("dof", dof, 6);
-    RCLCPP_INFO(node->get_logger(), "namespace: %s, dof: %d", node->get_namespace(), dof);
+  // Define the pose of the box (relative to the frame_id)
+  geometry_msgs::msg::Pose box_pose;
+  box_pose.orientation.w = 1.0;  // We can leave out the x, y, and z components of the quaternion since they are initialized to 0
+  box_pose.position.x = 0.34;
+  box_pose.position.y = 0.0;
+  box_pose.position.z = -0.045;
 
-    rclcpp::Client<xarm_msgs::srv::PlanPose>::SharedPtr pose_plan_client_ = node->create_client<xarm_msgs::srv::PlanPose>("xarm_pose_plan");
-    rclcpp::Client<xarm_msgs::srv::PlanExec>::SharedPtr exec_plan_client_ = node->create_client<xarm_msgs::srv::PlanExec>("xarm_exec_plan");
+  collision_object.primitives.push_back(primitive);
+  collision_object.primitive_poses.push_back(box_pose);
+  collision_object.operation = collision_object.ADD;
 
-    std::shared_ptr<xarm_msgs::srv::PlanPose::Request> pose_plan_req = std::make_shared<xarm_msgs::srv::PlanPose::Request>();;
-    std::shared_ptr<xarm_msgs::srv::PlanExec::Request> exec_plan_req = std::make_shared<xarm_msgs::srv::PlanExec::Request>();;
+  return collision_object;
+}();
 
-    exec_plan_req->wait = true;
+  // Add the collision object to the scene
+  moveit::planning_interface::PlanningSceneInterface planning_scene_interface;
+  planning_scene_interface.applyCollisionObject(collision_object);
 
-    geometry_msgs::msg::Pose target_pose1;
-    target_pose1.position.x = 0.3;
-    target_pose1.position.y = -0.1;
-    target_pose1.position.z = 0.2;
-    target_pose1.orientation.x = 1;
-    target_pose1.orientation.y = 0;
-    target_pose1.orientation.z = 0;
-    target_pose1.orientation.w = 0;
-
-    geometry_msgs::msg::Pose target_pose2;
-    target_pose2.position.x = 0.3;
-    target_pose2.position.y = 0.1;
-    target_pose2.position.z = 0.2;
-    target_pose2.orientation.x = 1;
-    target_pose2.orientation.y = 0;
-    target_pose2.orientation.z = 0;
-    target_pose2.orientation.w = 0;
-
-    geometry_msgs::msg::Pose target_pose3;
-    target_pose3.position.x = 0.3;
-    target_pose3.position.y = 0.1;
-    target_pose3.position.z = 0.4;
-    target_pose3.orientation.x = 1;
-    target_pose3.orientation.y = 0;
-    target_pose3.orientation.z = 0;
-    target_pose3.orientation.w = 0;
-
-    geometry_msgs::msg::Pose target_pose4;
-    target_pose4.position.x = 0.3;
-    target_pose4.position.y = -0.1;
-    target_pose4.position.z = 0.4;
-    target_pose4.orientation.x = 1;
-    target_pose4.orientation.y = 0;
-    target_pose4.orientation.z = 0;
-    target_pose4.orientation.w = 0;
-
-    // Add the collision object to the scene
-    moveit::planning_interface::PlanningSceneInterface planning_scene_interface;
-    planning_scene_interface.applyCollisionObject(collision_object);
-    
-    while (rclcpp::ok())
-    {
-        pose_plan_req->target = target_pose1;
-        call_request(pose_plan_client_, pose_plan_req);
-        call_request(exec_plan_client_, exec_plan_req);
-
-        pose_plan_req->target = target_pose2;
-        call_request(pose_plan_client_, pose_plan_req);
-        call_request(exec_plan_client_, exec_plan_req);
-
-        pose_plan_req->target = target_pose3;
-        call_request(pose_plan_client_, pose_plan_req);
-        call_request(exec_plan_client_, exec_plan_req);
-
-        pose_plan_req->target = target_pose4;
-        call_request(pose_plan_client_, pose_plan_req);
-        call_request(exec_plan_client_, exec_plan_req);
-    }
-
-    RCLCPP_INFO(node->get_logger(), "test_xarm_planner_node_pose over");
-    return 0;
+  planning_scene_interface.applyCollisionObject(collision_object_2);
+  
+  
+  // Shutdown ROS
+  rclcpp::shutdown();
+  return 0;
 }
